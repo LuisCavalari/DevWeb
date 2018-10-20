936e53d6debf99bf383325d198e43d6e3d99003d
Luis Cavalari <lfelipe.felipe4@gmail.com>
2018-10-19 22:43:27 -0300

    Refatoramento e adilçai de novos conceitos

---
+++ Js/fundamento/numberWarnings.js
@@ -0,0 +1,13 @@
+
+console.log(7/0) // não gera exceção, apenas ininito
+console.log("10" / 2) 
+//em JS é possivel dividir strings, mas desde que obedecem a condição numérica
+console.log("Alowww" * 2) //não é possivel multiplicar palavras
+
+console.log(0.1+0.8) //existe um arrendodamento que não é 100% preciso
+
+console.log((50.2555).toFixed(2)) // arrendondamento de um numero
+ /*
+ * Concluindo EcmaScript é simplismente uma das mais importante linguagens 
+ * já criadas
+ */
